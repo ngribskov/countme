639ab0a5a3cc8c9056b7d8531aebf0f75a8903b5
Niels <ngribskov@gmail.com>
2016-06-18 15:49:31 -0400

    added assocciates to models, simplified column add

--- CountMe/app/models/elected_through.rb
+++ CountMe/app/models/elected_through.rb
@@ -1,2 +1,4 @@
 class ElectedThrough < ActiveRecord::Base
+  belongs_to :voter
+  belongs_to :politician 
 end
--- CountMe/db/migrate/20160617191755_add_column_to_politician.rb
+++ CountMe/db/migrate/20160617191755_add_column_to_politician.rb
@@ -1,6 +1,5 @@
 class AddColumnToPolitician < ActiveRecord::Migration
   def change
     add_column :politicians, :district, :string
-    add_column :politicians, :job, :string
   end
 end
